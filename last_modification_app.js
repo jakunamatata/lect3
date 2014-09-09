3d4cd2a0574173d0d411ef543d9d6bfb6979340f
= <=>
2014-09-09 19:07:09 +0300

    Fix last modified corner case (UNIX time 0)

--- last_modification_app.js
+++ last_modification_app.js
@@ -12,7 +12,10 @@ var format = function(date) {
 }
 
 var handleDate = function(last_modified){
-	var formatted = format(new Date(last_modified));
+
+	var formatted; 
+	if(last_modified !== false) formatted = format(new Date(last_modified));	
+	else formatted = last_modified;
 	console.log(formatted);
 }
 mod.last_modification(filename, handleDate);
