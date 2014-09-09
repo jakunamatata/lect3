98801fae657bfc96b1e40fbf08927c7005d3bd35
= <=>
2014-09-09 18:27:52 +0300

    remove useless node_modules

--- last_modification.js
+++ last_modification.js
@@ -3,7 +3,6 @@ var fs = require("fs");
 exports.last_modification = function (filename, callback) {
 	fs.exists(filename, function(exists){
 		if(!exists) return callback(exists);
-
 		fs.stat(filename, function(err, stats){
 			callback(stats.mtime);
 		});
