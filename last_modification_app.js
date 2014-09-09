aa5439d661e24774334928ed0171fdaf3a4c4a9b
= <=>
2014-09-09 18:02:44 +0300

    Remove strftime because apparently using modules was not okay -.-

--- last_modification_app.js
+++ last_modification_app.js
@@ -1,8 +1,18 @@
 var mod = require("./last_modification");
-var strftime = require("strftime");
-
 var filename = process.argv[2];
+
+
+var format = function(date) {
+	return date.getHours() + ":" + 
+		   date.getMinutes() + " " +
+		   date.getDate() + "." +
+		   (date.getMonth()+1) + "." +
+		   date.getFullYear();
+
+}
+
+
 mod.last_modification(filename, function(last_modified){
-	var formatted = strftime("%H:%M %d.%m.%Y ", new Date(last_modified))
+	var formatted = format(new Date(last_modified));
 	console.log(formatted);
 });
